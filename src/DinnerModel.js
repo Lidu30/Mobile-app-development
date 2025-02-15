21fffec7e7c1a4f40ea8e1f9b97316ce14858791
Lidya <lhnasser@kth.se>
2025-02-15 23:01:59 +0100

    2.2.3, is failing some tests though

--- src/DinnerModel.js
+++ src/DinnerModel.js
@@ -2,38 +2,64 @@
    The Model keeps the state of the application (Application State). 
    It is an abstract object, i.e. it knows nothing about graphics and interaction.
 */
-export const model = {  
+
+import { getDishDetails, searchDishes } from "src/dishSource"
+import { resolvePromise } from "src/resolvePromise"
+
+export const model = {
   numberOfGuests: 2,
   dishes: [],
-  currentDishId: null,  // null means "intentionally empty"
+  currentDishId: null, // null means "intentionally empty"
+  searchParams: {},
+  searchResultsPromiseState: {},
+  currentDishPromiseState: {},
 
-  setCurrentDishId(dishId){
-      this.currentDishId = dishId
+  setCurrentDishId(dishId) {
+    this.currentDishId = dishId
   },
-  
-  setNumberOfGuests(number){
-      if(Number.isInteger(number) && number > 0) {
-          this.numberOfGuests = number;
-      }
-      else {
-          throw new Error("number of guests not a positive integer")
-      }
+
+  setNumberOfGuests(number) {
+    if (Number.isInteger(number) && number > 0) {
+      this.numberOfGuests = number
+    } else {
+      throw new Error("number of guests not a positive integer")
+    }
   },
-  
-  addToMenu(dishToAdd){
-      // array spread syntax exercise
-      // It sets this.dishes to a new array [   ] where we spread (...) the elements of the existing this.dishes
-      this.dishes = [...this.dishes, dishToAdd];
+
+  addToMenu(dishToAdd) {
+    // array spread syntax exercise
+    // It sets this.dishes to a new array [   ] where we spread (...) the elements of the existing this.dishes
+    this.dishes = [...this.dishes, dishToAdd]
   },
 
   // filter callback exercise
-  removeFromMenu(dishToRemove){
-      function shouldWeKeepDishCB(dish){
-          return dishToRemove.id !== dish.id
-      }
-      this.dishes = this.dishes.filter(shouldWeKeepDishCB);
+  removeFromMenu(dishToRemove) {
+    function shouldWeKeepDishCB(dish) {
+      return dishToRemove.id !== dish.id
+    }
+    this.dishes = this.dishes.filter(shouldWeKeepDishCB)
+  },
+
+  setSearchQuery(query) {
+    this.searchParams.query = query
+  },
+
+  setSearchType(type) {
+    this.searchParams.type = type
+  },
+
+  doSearch(params) {
+    const searchPromise = searchDishes(params)
+    resolvePromise(searchPromise, this.searchResultsPromiseState)
+  },
+
+  currentDishEffect() {
+    if (!this.currentDishId) {
+      this.currentDishPromiseState = { promise: null, data: null, error: null }
+    }
+    const dishPromise = getDishDetails(this.currentDishId)
+    resolvePromise(dishPromise, this.currentDishPromiseState)
   },
 
   // more methods will be added here, don't forget to separate them with comma!
-};
-
+}
--- src/bootstrapping.js
+++ src/bootstrapping.js
@@ -2,18 +2,28 @@ import "./teacherFetch"
 
 import { configure, observable, reaction } from "mobx"
 
+import { model } from "./DinnerModel"
 import { dishesConst } from "./dishesConst"
 
 //——————————————————————————————————————————————————————————————————————————————
 
 configure({ enforceActions: "never" })
 
-import {model} from "./DinnerModel";
-export const reactiveModel = observable(model);
+export const reactiveModel = observable(model)
 
 // make the model and a few example dishes available in the browser Console for testing
 global.myModel = reactiveModel
 global.dishesConst = dishesConst
+
+reaction(currentDishIdACB, currentDishEffectACB)
+
+function currentDishIdACB() {
+  return reactiveModel.currentDishId
+}
+
+function currentDishEffectACB() {
+  reactiveModel.currentDishEffect()
+}
 
 // or you can add a few dishes here, don't forget to remove this in TW2
 // myModel.numberOfGuests = 3
