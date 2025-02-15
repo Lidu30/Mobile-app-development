2489d28e2bcfd72ecfbd133acfb4499c7c116f42
Lidya <lhnasser@kth.se>
2025-02-15 17:59:10 +0100

    2.1.2 passing all tests, 2.1.3, failing 2 tests

--- src/dishSource.js
+++ src/dishSource.js
@@ -9,10 +9,36 @@ export function searchDishes(searchParams) {
             "X-DH2642-Key": PROXY_KEY,
             "X-DH2642-Group": 11,
         }
-    }).then(response => response.json()).then(someACB);
+    }).then(gotResponseACB).then(someACB);
 
     function someACB(response){
       return response.results;
     }
-   
+
+    function gotResponseACB(response){
+      return response.json();
+    }   
+}
+
+
+export function getMenuDetails(ids_array){
+  
+  const queryString = new URLSearchParams({ ids: ids_array});
+  const url = `${PROXY_URL}/recipes/informationBulk?${queryString}`;
+
+  return fetch(url, {
+    headers: {
+      "X-DH2642-Key": PROXY_KEY,
+      "X-DH2642-Group": 11,
+    }
+
+  }).then(gotResponseACB);
+  
+  function gotResponseACB(response){
+    if (!response.ok){
+      throw new Error(`HTTP Error! Status: ${response.status}`)
+    }
+    return response.json();
+  }
+
 }
