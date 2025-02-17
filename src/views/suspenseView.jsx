584e82dbfe3b1d8f734eabb6906b43f13c955dc1
Lidya <lhnasser@kth.se>
2025-02-17 16:49:53 +0100

    suspense view

--- src/views/suspenseView.jsx
+++ src/views/suspenseView.jsx
@@ -1,5 +1,49 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native"
+import { Image } from "expo-image"
 
 export function SuspenseView(props) {
-    return (<Text>SuspenseView</Text>);
+  const blurhash =
+    "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj["
+
+  if (props.error) {
+    return (
+      <View>
+        <Text> {props.error.toString()}</Text>
+      </View>
+    )
+  }
+
+  if (props.promise && !props.dishData) {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source="https://brfenergi.se/iprog/loading.gif"
+          placeholder={{ blurhash }}
+          contentFit="cover" //so that the image is resized to cover the container
+          transition={1000} //a smooth fade-in transition for the image once it has loaded. 1 sec is the duration of transition
+        />
+      </View>
+    )
+  }
+
+  return (
+    <View>
+      <Text>no data</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 50,
+    hight: 50,
+    backgroundColor: "#0553",
+  },
+})
