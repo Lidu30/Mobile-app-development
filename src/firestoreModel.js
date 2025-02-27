7e991fa60ca6a1d1fc0db0c7415cf05d7b90b903
Lidya <lhnasser@kth.se>
2025-02-27 03:26:19 +0100

    3.1.2, 2 tests passing

--- src/firestoreModel.js
+++ src/firestoreModel.js
@@ -1,36 +1,40 @@
-import { initializeApp } from "firebase/app";
-import {firebaseConfig} from "src/firebaseConfig.js";
-const app= initializeApp(firebaseConfig);
-
+import { initializeApp } from "firebase/app"
 // initialize Firestore
-import {getFirestore, doc, setDoc, getDoc} from "firebase/firestore";
-const db= getFirestore(app);
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore"
+import { firebaseConfig } from "src/firebaseConfig.js"
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
 
 // make doc and setDoc available at the Console for testing
-global.doc= doc        
-global.setDoc= setDoc
-global.db= db
+global.doc = doc
+global.setDoc = setDoc
+global.db = db
 
 /* Replace NN with your TW2_TW3 group number! */
-const COLLECTION="dinnerModel11";
+const COLLECTION = "dinnerModel11"
 
-export function connectToPersistence(reactiveModel, reaction) {
-    
-    function getModelStateACB() {
-        return {
-            numberOfGuests: reactiveModel.numberOfGuests,
-            dishes: reactiveModel.dishes,
-            currentDish: reactiveModel.currentDish,
-        };
+export function connectToPersistence(model, watchFunction) {
+  function getModelStateACB() {
+    return [model.numberOfGuests, model.dishes, model.currentDishId]
+  }
+
+  function persistenceModelACB() {
+    const refObject = doc(db, COLLECTION, "modelData")
+    if (model.ready) {
+        setDoc(
+        refObject,
+        {
+          numberOfGuests: model.numberOfGuests,
+          dishes: model.dishes,
+          currentDishId: model.currentDishId,
+        },
+        { merge: true },
+      )
     }
+  }
 
-    function persistenceModelACB(modelState){
-        const refObject = doc(db,COLLECTION, "modelData");
-        setDoc(refObject, modelState, {merge:true});
-    }
 
-    reaction(getModelStateACB, persistenceModelACB);
-    
+  watchFunction(getModelStateACB, persistenceModelACB)
 }
-
-
